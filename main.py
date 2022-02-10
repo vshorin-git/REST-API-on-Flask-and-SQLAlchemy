b2df8357fb75d8fc409b51c8b227f340d0d3bc0a
vshorin-git <Mesedalg@gmail.com>
2022-02-10 21:54:41 +0300

    Finalized CRUD methods

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -40,11 +40,34 @@ class PostListResource(Resource):
         return post_schema.dump(new_post)
 
 
+class PostResource(Resource):
+    def get(self, post_id):
+        post = Post.query.get_or_404(post_id)
+        return post_schema.dump(post)
+
+    def patch(self, post_id):
+        post = Post.query.get_or_404(post_id)
+
+        if 'title' in request.json:
+            post.title = request.json['title']
+        if 'content' in request.json:
+            post.content = request.json['content']
+
+        db.session.commit()
+        return post_schema.dump(post)
+
+    def delete(self, post_id):
+        post = Post.query.get_or_404(post_id)
+        db.session.delete(post)
+        db.session.commit()
+        return '', 204
+
+
 api.add_resource(PostListResource, '/posts')
+api.add_resource(PostResource, '/posts/<int:post_id>')
 
 post_schema = PostSchema()
 posts_schema = PostSchema(many=True)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
